386cca098362079a3eb56b63dd5a2f67fcecfdab
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-23 19:04:04 +0100

    feat: relationships between Models are added in index.models.js

--- app/models/index.models.js
+++ app/models/index.models.js
@@ -2,4 +2,28 @@ import { User } from "./User.model.js";
 import { Chat } from "./Chat.model.js";
 import { Message } from "./Message.model.js";
 
+// Define relationships between the models : 
+
+// Relation between User and Chat 
+User.hasMany(Chat, {
+    foreignKey: "user_id",
+    as: "chats"
+});
+
+Chat.belongsTo(User, {
+    foreignKey: "user_id",
+    as: "user"
+});
+
+// Relation between Chat and Message
+Chat.hasMany(Message, {
+    foreignKey: "chat_id",
+    as: "messages"
+});
+
+Message.belongsTo(Chat, {
+    foreignKey: "chat_id",
+    as: "chat"
+})
+
 export { User, Chat, Message };
