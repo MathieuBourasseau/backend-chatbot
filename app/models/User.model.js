c6a1cf9db1ccb40f671c9c60b05994bd890ad768
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2025-12-23 18:32:04 +0100

    feat: index.models.js created to gather all models at the same file + User model created

---
+++ app/models/User.model.js
@@ -0,0 +1,20 @@
+import { sequelize } from "./sequelize.client.js";
+import { DataTypes, Model } from "sequelize"
+
+// Creation of User model from sequelize 
+export class User extends Model {}
+
+User.init(
+    {
+        identifier: {
+            type: DataTypes.TEXT, //This data is a string type without limitation of characters
+            allowNull: false, // Identifier is obligatory to access to conversation
+            unique: true, // Identifier must be unique for each user
+        }
+    },
+    {
+        sequelize, // Information required to connect to the DB
+        modelName: "User", // Name of the model
+        tableName: "user", // Name of the table in the database
+    }
+);
