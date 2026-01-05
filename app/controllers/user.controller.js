b7d2885f1d0a730795757cb8f0c92b38e496450c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-05 15:08:09 +0100

    feat: userController created

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,9 +5,21 @@ export const userController = {
     getOrCreate: async (req, res) => {
 
         try {
+
+            // Catch the identifier from the request
+            const { identifier } = req.params;
+
+            // Find or create new identifier in the DB
+            const [user, created ] = await User.findOrCreate({
+                where: { identifier }, // Where to search in the DB
+                defaults: { identifier } // Which identifier to create if it is not existing in the DB
+            });
             
+            return res.status(created ? 201 : 200).json(user) // Response with json object 
+
         } catch (error) {
-            
+            console.error(error);
+            return res.status(500).json({ error : "Un problème est survenu du côté du serveur..."})
         }
     }
 }
