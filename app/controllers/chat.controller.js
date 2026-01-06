a74067f88f3ec9ce3926c67bd64d6ab1c1aec511
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-06 11:14:14 +0100

    feat: chat.router.js updated with a new route to create chat + beginning of the method createChat in chat.controller.js

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -2,6 +2,7 @@ import { Chat } from "../models/index.models.js"
 
 export const chatController = {
 
+    // Method to get all the chats
     getAll : async (req, res) => {
 
         try {
@@ -9,6 +10,18 @@ export const chatController = {
             res.json(chats)
         } catch (error) {
             res.status(500).json({ error: "Erreur lors de la récupération des discussions."})
+        }
+    },
+
+    // Method to create a new chat 
+    createChat: async (req, res) => {
+
+        try {
+            
+            const { firstMessage, user_id } = req.body;
+
+        } catch (error) {
+            
         }
     }
 }
