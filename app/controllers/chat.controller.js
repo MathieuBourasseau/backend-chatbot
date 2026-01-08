62989c43bc3ac6e216819af7225859f1227932da
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-08 18:23:53 +0100

    feat: create a new chat with the title given by Mistral and the user id

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -1,15 +1,16 @@
 import { Chat } from "../models/index.models.js"
+import 'dotenv/config'
 
 export const chatController = {
 
     // Method to get all the chats
-    getAll : async (req, res) => {
+    getAll: async (req, res) => {
 
         try {
             const chats = await Chat.findAll();
             res.json(chats)
         } catch (error) {
-            res.status(500).json({ error: "Erreur lors de la récupération des discussions."})
+            res.status(500).json({ error: "Erreur lors de la récupération des discussions." })
         }
     },
 
@@ -17,11 +18,43 @@ export const chatController = {
     createChat: async (req, res) => {
 
         try {
-            
-            const { firstMessage, user_id } = req.body;
+
+            const { firstMessage, user_id } = req.body; // Catch first message and user id from the body request
+
+            // Fetch to mistral 
+
+            const response = await fetch('https://api.mistral.ai/v1/chat/completions', {
+                method: 'POST', // method to send content
+                headers: {
+                    'Content-type' : 'application/json', // JSON content 
+                    'Authorization' : `Bearer ${process.env.MISTRAL_API_KEY}` // API key required to use Mistral 
+                },
+                body: JSON.stringify({
+                    model: "mistral-small-latest",
+                    messages: [
+                        {
+                            role: "system",
+                            content: "Tu es un assistant spécialisé dans la synthèse. Ta seule tâche est de générer un titre court (3 à 5 mots maximum) basé sur le message de l'utilisateur. Ne réponds pas au message, donne uniquement le titre sans ponctuation."
+                        },
+                        {
+                            role: "user",
+                            content: `${firstMessage}`
+                        }
+                    ]
+                })
+            })
+
+            const data = await response.json();
+            const chatTitle = data.choices[0].message.content;
+
+            // Create a new chat with the chat title generated before and the user id
+            const newChat = await Chat.create({
+                name: chatTitle,
+                user_id: user_id,
+            })
 
         } catch (error) {
-            
+
         }
     }
 }
