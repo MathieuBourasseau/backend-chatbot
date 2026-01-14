fd22b6ff154730ac10673587c171d1fa677c5cc3
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-14 17:48:39 +0100

    feat: create a new route to continue a chat + create the function checkChat

--- app/middlewares/checkChat.js
+++ app/middlewares/checkChat.js
@@ -1,7 +1,8 @@
 import Joi from "joi"
 import { chatSchema } from "../schemas/chat.schema.js";
 
-export function checkChat(req, res, next) {
+// Middleware to check the first message of chat
+export function checkFirstMessage(req, res, next) {
 
     try {
 
@@ -11,5 +12,15 @@ export function checkChat(req, res, next) {
 
     } catch (error) {
         return res.status(400).json({ error: "Le premier message est trop court pour générer un titre. ❌"})
+    }
+};
+
+// Middleware to check the chat messages
+export function checkChat(req, res, next) {
+
+    try {
+        Joi.attempt()
+    } catch (error) {
+        
     }
 }
