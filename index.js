cc86aed01cbb0f8dc504d04614714c2e13aff19c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-16 11:58:49 +0100

    feat: added a new url for CORS policy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 // CORS policy authorization
-app.use(cors({ origin: 'http://localhost:5173'}));
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173']}));
 
 // Transform JSON data from the front in javascript object usable in controller
 app.use(express.json()); 
