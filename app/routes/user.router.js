c60f8f83abb0997669aac84b871d42b34926ef0c
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-19 09:14:33 +0100

    feat: former route deleted in user.router.js + two routes created to signup and to login

--- app/routes/user.router.js
+++ app/routes/user.router.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import { checkIdentifier } from "../middlewares/checkIdentifier.js"
 import { userController } from "../controllers/user.controller.js";
 
+// Create the router
 export const userRouter = Router();
 
-// Route to identify the user 
-userRouter.get('/user/:identifier', checkIdentifier, userController.getOrCreate)
+// Route to register user 
+userRouter.post('/signup', userController.registerUser );
+
+// Route to log a user 
+userRouter.post('/login', userController.logUser);
