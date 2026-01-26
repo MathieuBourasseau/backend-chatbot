1e0f23c90152dce2949afdeab227bce49fbe8eca
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-26 19:40:12 +0100

    feat: filename method created in multer-config.js + export multer

--- app/middlewares/multer-config.js
+++ app/middlewares/multer-config.js
@@ -11,9 +11,24 @@ const MIME_TYPES = {
 // --- METHOD TO GET, SAVE AND RENAME THE FILES RECEIVED --- 
 const storage = multer.diskStorage({
 
-    // Where to save the files received
-    destination: (req, file, callback) {
+    // Save the files in the uploads directory
+    destination: (req, file, callback) => {
         callback(null, 'uploads');
+    },
+
+    // Method to rename the file 
+    filename: (req, file, callback) => {
+
+        // Get the original file name by replacing spaces by underscore and removing the extension
+        const name = file.originalname.split(' ').join('_').split('.')[0];
+
+        // Find the right extension
+        const extension = MIME_TYPES[file.mimetype];
+
+        // Create the final name 
+        callback(null, name, Date.now() + '.' + extension)
     }
-})
+});
+
+export default multer({ storage: storage }).single('avatar');
 
