8ab20c0d9cb67a8379bfb74128ca4bef21beb4d4
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-26 19:29:36 +0100

    feat: multer-config.js created + multer imported in this middleware + Mime_types variable created

---
+++ app/middlewares/multer-config.js
@@ -0,0 +1,10 @@
+import multer from "multer";
+
+// --- FILES ACCEPTED BY BACKEND --- 
+const MIME_TYPES = {
+    'image/jpeg' : 'jpg',
+    'image/jpg' : 'jpg',
+    'image/png' : 'png',
+    'image/webp' : 'web'
+}
+
