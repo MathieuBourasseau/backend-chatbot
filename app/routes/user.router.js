54220772e189267b0526eade01b3d87ee71e84dc
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-27 09:43:16 +0100

    feat: multerConfig imported in user.router.js + multerConfig middleware added before userController to handle image file

--- app/routes/user.router.js
+++ app/routes/user.router.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { userController } from "../controllers/user.controller.js";
+import multerConfig from "../middlewares/multer-config.js";
+
 
 // Create the router
 export const userRouter = Router();
 
 // Route to register user 
-userRouter.post('/signup', userController.registerUser );
+userRouter.post('/signup', multerConfig, userController.registerUser);
 
 // Route to log a user 
 userRouter.post('/login', userController.logUser);
