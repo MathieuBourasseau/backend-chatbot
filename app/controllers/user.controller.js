f11dae62889ad2a024712de2c778cb4cefa8a5cc
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-28 19:48:22 +0100

    feat: secure added in transporter

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -172,6 +172,7 @@ export const userController = {
             const transporter = nodemailer.createTransport({
                 host: process.env.EMAIL_HOST,
                 port: process.env.EMAIL_PORT,
+                secure: false,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS,
