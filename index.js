9fae17863c74bc28f7ed9191033f502a7eeacbb0
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-01-28 15:08:40 +0100

    feat: test to deploy

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ async function startServer() {
 // AUTHORIZATION TO ACCESS UPLOADS DIRECTORY FOR AVATARS
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
+app.get('/hello', (req, res) => res.json({ message: "Le serveur est bien vivant !" }));
+
 // API ROUTE
 app.use('/api', apiRouter);
 
